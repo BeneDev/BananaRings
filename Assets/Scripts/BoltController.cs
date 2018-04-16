f74d3fb2cc5e06d1692de24def22859104e9a382
Bene_Dev <seriousbenedikt@web.de>
2018-04-16 21:48:27 +0200

    Bolts Collide with obstacles and enemies

--- Assets/Scripts/BoltController.cs
+++ Assets/Scripts/BoltController.cs
@@ -5,9 +5,39 @@ using UnityEngine;
 public class BoltController : MonoBehaviour {
 
     [SerializeField] float speed = 10f;
+    public LayerMask collideLayers;
 
-	// Update is called once per frame
-	void Update () {
+    private void Awake()
+    {
+        int enemyLayer = LayerMask.NameToLayer("Enemies");
+        int obstacleLayer = LayerMask.NameToLayer("Obstacles");
+        collideLayers = 1 << enemyLayer;
+        LayerMask obstLayerMask = 1 << obstacleLayer;
+        collideLayers = collideLayers | obstLayerMask;
+    }
+
+    // Update is called once per frame
+    void Update () {
         transform.position += transform.forward * speed * Time.deltaTime;
+        StartCoroutine(DestroyAfterTime(3f));
 	}
+
+    private void FixedUpdate()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, 1.4f, collideLayers))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator DestroyAfterTime(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.DrawRay(transform.position, transform.forward * 1.4f);
+    //}
 }
