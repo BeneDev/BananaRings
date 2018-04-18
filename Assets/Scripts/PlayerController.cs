483c2e913f9f674b3ed657b8786510f1ec0a10ad
BeneDev <seriousbenedikt@web.de>
2018-04-18 15:21:23 +0200

    Reorganised the PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// This script Controls the Player, reading the Input from a PlayerInput.
+/// </summary>
 [DisallowMultipleComponent]
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
@@ -9,6 +13,7 @@ public class PlayerController : MonoBehaviour
 
     #region Properties
 
+    // The velocity of the Rigidbody
     public Vector3 Velocity
     {
         get
@@ -17,6 +22,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // The direction to shoot in. Uses the input from the right analog stick.
     public Vector3 ShootDirection
     {
         get
@@ -29,38 +35,37 @@ public class PlayerController : MonoBehaviour
 
     #region Fields
 
-    [Header("Movement"), SerializeField] float speed = 1f;
-    [SerializeField] float boostAmount = 2.5f;
-    [SerializeField] float veloCap = 300f;
-    Vector3 velocity;
-    [SerializeField] float rotationSpeed = 1f;
-
+    // Components attached to the player
     PlayerInput input;
-
-    [Header("Physics"), SerializeField] float lookDistance = 2f;
-    LayerMask obstacles;
-    [SerializeField] float colliderRadius = 1f;
-
-    [Header("Shooting"), SerializeField] Transform[] guns;
-    [SerializeField] GameObject gunObject;
-    [SerializeField] GameObject bolt;
-    [SerializeField] float shotDelay = 0.5f;
-    float shotCounter = 0f;
-    Vector3 shootDirection;
-
     Rigidbody rb;
 
+    // Boolean Fields
+    bool bBoostMode = false;
+
+    [Header("Movement"), SerializeField] float speed = 1f; // The speed, the player moves with
+    [SerializeField] float boostAmount = 2.5f; // How much faster the player goes when boosting
+    [SerializeField] float veloCap = 300f; // How fast the player can possibly go
+    Vector3 direction; // Stores the direction, the player wants to move in
+    [SerializeField] float rotationSpeed = 1f; // How fast the player can turn
+    
+    [Header("Shooting"), SerializeField] Transform[] guns; // The transforms of the different guns to shoot out of
+    [SerializeField] GameObject gunObject; // The gun object which gets rotated depending on where the player aims
+    [SerializeField] GameObject bolt; // The object which gets shot
+    [SerializeField] float shotCooldown = 0.5f; // The cooldown between the different shots
+    float shotCounter = 0f; // The counter to tick down between the shots. Only if this one is zero, the player can shoot
+    Vector3 shootDirection; // The direction, the player aims at
+
     // Attributes of the particle effect
+    // TODO store the different trailEffects in an array as well as the modules
     [Header("ParticleEffect"), SerializeField] ParticleSystem trailEffect;
     [SerializeField] ParticleSystem secondTrail;
     ParticleSystem.MainModule main;
     ParticleSystem.EmissionModule emission;
     ParticleSystem.MainModule secMain;
     ParticleSystem.EmissionModule secEmission;
+    // TODO get the default size from the particle system itself
     [SerializeField] float defaultSize;
     [SerializeField] float defaultRate;
-
-    bool bBoostMode = false;
 
     #endregion
 
@@ -68,18 +73,17 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        // Get the references of the Particle Systems for the thrust.
         emission = trailEffect.emission;
         main = trailEffect.main;
         secMain = secondTrail.main;
         secEmission = secondTrail.emission;
+
+        // Get the player components needed.
         input = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody>();
-
-        int layer = LayerMask.NameToLayer("Obstacles");
-        obstacles = 1 << layer;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         ManageBoostTrail();
@@ -88,6 +92,8 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         ReadInput();
+
+        // Shoot if the cooldown is worn off
         if(shotCounter > 0)
         {
             shotCounter -= Time.fixedDeltaTime;
@@ -100,16 +106,18 @@ public class PlayerController : MonoBehaviour
 
     #region Methods
 
+    // Make the player move and change the thrust particle system depending on wether the player boosts or not
     void Thrust(bool mode)
     {
+        // When in boost mode
         if (mode)
         {
-            //transform.position += velocity * (speed + input.Boost * boostAmount) * Time.fixedDeltaTime;
+            // Add velocity as long as the velocity Cap is not reached
             if (rb.velocity.magnitude < veloCap)
             {
-                rb.velocity += velocity * (speed + input.Boost * boostAmount) * Time.fixedDeltaTime;
+                rb.velocity += direction * (speed + input.Boost * boostAmount) * Time.fixedDeltaTime;
             }
-            //rb.AddForce(velocity * (speed + input.Boost * boostAmount) * Time.fixedDeltaTime);
+            // Set the trail particle effects to emit bigger particles faster
             main.startSizeMultiplier = 8f;
             emission.rateOverDistance = 3f;
             secMain.startSizeMultiplier = 8f;
@@ -117,12 +125,12 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            //transform.position += velocity * speed * Time.fixedDeltaTime;
+            // Add velocity as long as the velocity Cap is not reached
             if (rb.velocity.magnitude < veloCap * 0.125f)
             {
-                rb.velocity += velocity * speed * Time.fixedDeltaTime;
+                rb.velocity += direction * speed * Time.fixedDeltaTime;
             }
-            //rb.AddForce(velocity * speed * Time.fixedDeltaTime);
+            // Set the trail particle effect values back to the default ones
             main.startSizeMultiplier = defaultSize;
             emission.rateOverDistance = defaultRate;
             secMain.startSizeMultiplier = defaultSize;
@@ -130,7 +138,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Sets the rotation of the trail particle effect
+    // Set the rotation of the trail particle effect
     private void ManageBoostTrail()
     {
         Quaternion trailRotation = Quaternion.LookRotation(-transform.forward);
@@ -138,14 +146,18 @@ public class PlayerController : MonoBehaviour
         trailEffect.transform.rotation = trailRotation;
     }
 
+    // Handles the input to manipulate the player.
     void ReadInput()
     {
-        velocity.x = input.Horizontal;
-        velocity.z = input.Vertical;
+        // Get the direction of the left stick
+        direction.x = input.Horizontal;
+        direction.z = input.Vertical;
 
+        // Get the direction of the right stick
         shootDirection.x = input.RightHorizontal;
         shootDirection.z = input.RightVertical;
 
+        // Set boostMode field
         if (input.Boost > 0f)
         {
             bBoostMode = true;
@@ -155,20 +167,22 @@ public class PlayerController : MonoBehaviour
             bBoostMode = false;
         }
 
+        // Rotate the player smoothly, depending on the velocity
         if (input.Horizontal != 0 || input.Vertical != 0)
         {
-            //transform.forward = velocity;
             Quaternion targetRotation = new Quaternion();
-            targetRotation.SetLookRotation(velocity); 
+            targetRotation.SetLookRotation(direction); 
 
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
         }
 
+        // Rotate the guns on the ship depending on the input of the right stick
         if (input.RightHorizontal != 0 || input.RightVertical != 0 && gunObject)
         {
             gunObject.transform.forward = shootDirection;
         }
 
+        // Instantiates a bullet when the player shoots
         if (input.Shoot)
         {
             if (guns.Length > 0 && bolt && shotCounter <= 0f)
@@ -177,7 +191,7 @@ public class PlayerController : MonoBehaviour
                 {
                     Instantiate(bolt, gun.position, gunObject.transform.rotation);
                 }
-                shotCounter = shotDelay;
+                shotCounter = shotCooldown;
             }
         }
     }
